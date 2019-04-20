6eb53c2fc526d4958f02eea58fb25ef7a1faade3
Joe <47788638+josephhearnshaw@users.noreply.github.com>
2019-04-20 21:20:41 +0100

    fixed filesize and formatting

--- list_files.py
+++ list_files.py
@@ -17,17 +17,14 @@ with open(str(sys.argv[1]), "r") as repomap:
     for line in repomap:
         # Split the line
         line = line.split("\t")
-            # Save the name of the file on its corresponding dataset.
+        # Save the name of the file on its corresponding dataset.
         dataset_dict[line[1]].append("{} ({})".format(
-								line[0], line[4].rstrip()))
-
-# Close the repomap
-repomap.close()
+            line[0], line[4].rstrip()))
 
 # Print a table with the files for each dataset. Loop through all the datasets.
 for dataset in dataset_dict.keys():
     # Print dataset table header
-    print("\n\t------\n\t|\n\t| " + dataset + " files:\n\t|")
+    print("\n\t------\n\t|\n\t| {} files:\n\t|".format(dataset))
     # If the dataset has no files
     if(len(dataset_dict[dataset]) == 0):
         # Inform the user the dataset is empty
@@ -35,5 +32,5 @@ for dataset in dataset_dict.keys():
     else:
         # Loop through the files contained in the dataset
         for file_name in dataset_dict[dataset]:
-            print("\t|\t" + file_name)
+            print("\t|\t{}".format(file_name))
     print("\t|\n\t------")
